e228ef3cc75a5791b5a0c651e7630e3d90277682
samuel <damosisamuel@gmail.com>
2021-01-07 17:21:57 +0100

    init + done

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $('.header-right > a').click(
+
+    function(){
+      $('.hamburger-menu').show();
+    }
+  )
+
+
+  $('.fa-times').click(
+
+  function(){
+    $('.hamburger-menu').hide();
+  }
+)
+
+});
